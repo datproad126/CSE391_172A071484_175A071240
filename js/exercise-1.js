ff340158bcff0f07ae291470119b77bbecc16227
dat ngo doan <datngodoan123@gmail.com>
2019-04-29 10:11:55 +0700

    baitap01

---
+++ js/exercise-1.js
@@ -0,0 +1,28 @@
+(function ($) {
+
+    $('[data-toggle="dropdown"]').dropdown();
+    $('.student').popover();
+    $.ajax({
+        dataType: "json",
+        url: "/file.json",
+        data: {},
+        success: function(data){
+
+            $.each(data, function(idx, item) {
+                
+                var $button = $('<button class="btn btn-secondary student" style=""></button>');
+                $button.attr('data-toggle', 'popover');
+                $button.attr('data-container', 'body');
+                $button.attr('data-placement', 'top');
+                $button.attr('data-content', "masv: " + item.id + ", nhom: " + item.group);
+                $button.text(item.name);
+                $button.popover();
+                $button.appendTo($("#class_container"));
+
+            });
+            
+        }
+    });
+        
+
+})(jQuery);
---
+++ js/exercise-2.js
@@ -0,0 +1,1 @@
+$('.tt-a').tooltip('enable');
