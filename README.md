af29b16dad41aefa3ece63f708a17036dd931a79
dat ngo doan <datngodoan123@gmail.com>
2019-04-29 15:47:46 +0700

    fix index.html ,baitap01.html

--- README.md
+++
@@ -1,8 +0,0 @@
-NỀN TẢNG VÀ PHÁT TRIỂN WEB
-
-team-design: phucdatbich
-gồm có : 
-  -sinh viên: Ngô Doãn Đạt
-  -sinh viên: Tô Thị Phúc
-
-giáo viên hướng dẫn: Kiều Tuấn Dũng 
--- js/exercise-1.js
+++ js/exercise-1.js
@@ -1,28 +1,29 @@
-(function ($) {
+$(function(){
 
-    $('[data-toggle="dropdown"]').dropdown();
-    $('.student').popover();
-    $.ajax({
-        dataType: "json",
-        url: "/file.json",
-        data: {},
-        success: function(data){
+    // $('[data-toggle="dropdown"]').dropdown();   
+    $('[data-toggle="popover"]').popover();
+    
+    // $.ajax({
+    //     dataType: "json",
+    //     url: "/file.json",
+    //     data: {},
+    //     success: function (data) {
 
-            $.each(data, function(idx, item) {
-                
-                var $button = $('<button class="btn btn-secondary student" style=""></button>');
-                $button.attr('data-toggle', 'popover');
-                $button.attr('data-container', 'body');
-                $button.attr('data-placement', 'top');
-                $button.attr('data-content', "masv: " + item.id + ", nhom: " + item.group);
-                $button.text(item.name);
-                $button.popover();
-                $button.appendTo($("#class_container"));
+    //         $.each(data, function (idx, item) {
 
-            });
-            
-        }
-    });
-        
+    //             var $button = $('<button class="btn btn-secondary student" style=""></button>');
+    //             $button.attr('data-toggle', 'popover');
+    //             $button.attr('data-container', 'body');
+    //             $button.attr('data-placement', 'top');
+    //             $button.attr('data-content', "masv: " + item.id + ", nhom: " + item.group);
+    //             $button.text(item.name);
+    //             $button.popover();
+    //             $button.appendTo($("#class_container"));
+
+    //         });
+
+    //     }
+    // });
+
 
 })(jQuery);
